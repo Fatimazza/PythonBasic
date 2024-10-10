fb215ae29d0be8127b366bf2dae77ccb75b0fe7b
Fatima Azzahro <azzahro.fatima@gmail.com>
2024-10-10 22:40:23 +0700

    Basic - Add Variable and Assignment with expression

--- 01_basic.py
+++ 01_basic.py
@@ -4,3 +4,7 @@ print("Hello World!")
 # variable and assignments
 greeting = "Hello Fatima" # assignment with value
 print(greeting)
+
+addition = 2+2 # assignment with expression
+result = addition - 1
+print(result)

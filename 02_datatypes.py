8d9030977268a55265cc5e809b3958c1734192f6
Fatima Azzahro <azzahro.fatima@gmail.com>
2024-10-11 12:20:04 +0700

    Data Types - Check Primitive are Immutable

--- 02_datatypes.py
+++ 02_datatypes.py
@@ -3,6 +3,13 @@ primitive data types
 consist of: numbers, boolean, string
 """
 
+# immutable
+print("\nPrimitive Data Types are Immutable")
+x = 1
+print(id(x), " ", type(x))
+x = "oke"
+print(id(x), " ", type(x))
+
 """
 collection data types
 consist of: list, tuple, set, dictionary
